be77354a56bcc26b476dbd8e4d616db56dc57293
Aleksander Malcew <amalcew@protonmail.com>
2021-03-22 20:48:57 +0100

    merge_sort() function added

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -3,7 +3,6 @@ def bubble_sort(lst):
         for j in range(0, len(lst)-1):
             if lst[j+1] < lst[j]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
-    return lst
 
 
 def selection_sort(lst):
@@ -13,17 +12,16 @@ def selection_sort(lst):
             if lst[j] < lst[minimal]:
                 minimal = j
         lst[minimal], lst[i] = lst[i], lst[minimal]
-    return lst
 
 
 def insertion_sort(lst):
-    sorted_lst = []
-    for i in range(0, len(lst)):
-        sorted_lst.append(lst[i])
-        for j in range(len(sorted_lst)-1):
-            if sorted_lst[-1] < sorted_lst[j]:
-                sorted_lst[j], sorted_lst[-1] = sorted_lst[-1], sorted_lst[j]
-    return sorted_lst
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i - 1
+        while j >= 0 and key < lst[j]:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
 
 
 def heapify(lst, n, index):
@@ -54,12 +52,38 @@ def heap_sort(lst):
     for i in range(n - 1, 0, -1):
         # swap root with last element
         lst[i], lst[0] = lst[0], lst[i]
-        # append max element and pop it from the heap
-        sorted_lst.append(lst.pop(i))
         # heapify the root element
         heapify(lst, i, 0)
 
-    # append the remaining root element
-    sorted_lst.append(lst[i - 1])
-    # reverse the sorted list
-    return sorted_lst[::-1]
+
+def merge_sort(lst):
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        left_lst = lst[:mid]
+        right_lst = lst[mid:]
+
+        merge_sort(left_lst)
+        merge_sort(right_lst)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_lst) and j < len(right_lst):
+            if left_lst[i] <= right_lst[j]:
+                lst[k] = left_lst[i]
+                i = i+1
+            else:
+                lst[k] = right_lst[j]
+                j = j+1
+            k = k+1
+
+        while i < len(left_lst):
+            lst[k] = left_lst[i]
+            i = i+1
+            k = k+1
+
+        while j < len(right_lst):
+            lst[k] = right_lst[j]
+            j = j+1
+            k = k+1
+
